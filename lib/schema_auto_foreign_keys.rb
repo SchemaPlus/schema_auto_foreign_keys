b6817913cf3b967512269a433eeadf1c52f5de43
ronen barzel <ronen@barzel.org>
2015-05-18 06:00:37 -0700

    move files into place, adjust paths

--- lib/schema_auto_foreign_keys.rb
+++ lib/schema_auto_foreign_keys.rb
@@ -1,13 +1,31 @@
-require 'schema_plus/core'
+require 'schema_plus/foreign_keys'
 
-require_relative 'schema_auto_foreign_keys/version'
+require_relative 'schema_auto_foreign_keys/active_record/migration/command_recorder'
+require_relative 'schema_auto_foreign_keys/middleware/migration'
+require_relative 'schema_auto_foreign_keys/middleware/schema'
 
-# Load any mixins to ActiveRecord modules, such as:
-#
-#require_relative 'schema_auto_foreign_keys/active_record/base'
+module SchemaAutoForeignKeys
+  module ActiveRecord
+    module ConnectionAdapters
+      autoload :Sqlite3Adapter, 'schema_auto_foreign_keys/active_record/connection_adapters/sqlite3_adapter'
+    end
+  end
+end
 
-# Load any middleware, such as:
-#
-# require_relative 'schema_auto_foreign_keys/middleware/model'
+class SchemaPlus::ForeignKeys::Config
+    ##
+    # :attr_accessor: auto_create
+    #
+    # Whether to automatically create foreign key constraints for columns
+    # suffixed with +_id+.  Boolean, default is +true+.
+    has_value :auto_create, :klass => :boolean, :default => true
+
+    ##
+    # :attr_accessor: auto_index
+    #
+    # Whether to automatically create indexes when creating foreign key constraints for columns.
+    # Boolean, default is +true+.
+    has_value :auto_index, :klass => :boolean, :default => true
+end
 
 SchemaMonkey.register SchemaAutoForeignKeys
